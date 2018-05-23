eee681c18f6c336c2bfe90d1b1e21a84c6c5631c
Ludovic DEDIER <ldedier@e2r11p18.42.fr>
2018-05-23 05:40:44 +0200

    full debug

--- SDL2-2.0.8/build/SDL_stdlib.d
+++
@@ -1,8 +0,0 @@
-build/SDL_stdlib.lo: \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/SDL_stdlib.c \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/../SDL_internal.h \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/../dynapi/SDL_dynapi.h \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/../libm/math_libm.h
