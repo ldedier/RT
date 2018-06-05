6764fd09876e1937a6e8fcf00b1b3cef3eb4af51
Ludovic DEDIER <ldedier@e2r11p5.42.fr>
2018-06-05 21:46:03 +0200

    menache

--- SDL2-2.0.8/build/.libs/SDL_string.d
+++
@@ -1,7 +0,0 @@
-build/SDL_string.lo: \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/SDL_string.c \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/../SDL_internal.h \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/../dynapi/SDL_dynapi.h \
-  /Users/ldedier/Projects/RT1/SDL2-2.0.8/src/stdlib/../dynapi/SDL_dynapi_overrides.h \
-  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
-  include/close_code.h include/SDL_stdinc.h
